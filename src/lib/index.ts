cd10ee321ec40d6a21f2b6dc039c5cd4e710e5d2
Aleksei Saenko <156539224+asaenko01@users.noreply.github.com>
2026-02-03 11:17:54 +0000

    wip

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import { createBarcodeScanner } from './create-barcode-scanner'
 
+export { createWorker } from './create-worker'
 export * from './utils'
 export { createBarcodeScanner }
 export default createBarcodeScanner
